faefe27ae51b7efe25d112732259e66978ce3a29
Tom MacWright <tom@macwright.org>
2013-11-26 13:31:29 -0500

    Add pong parts

--- ubuntu.bash
+++ ubuntu.bash
@@ -3,26 +3,36 @@
 function installDependencies
 {
     apt-get update
+    apt-add-repository -y ppa:chris-lea/node.js
     apt-get -y upgrade
     apt-get -y install build-essential
-    apt-get -y nodejs
+    apt-get -y install nodejs
 }
 
 function installStatSD
 {
+    mkdir -p /etc/statsd/
+    mkdir -p /etc/pong/
+    cp ./config/config.js '/etc/statsd/localConfig.js'
+    cp ./config/pong.json '/etc/pong/config.json'
+    cp ./files/statsd.conf '/etc/init/statsd.conf'
+    cp ./files/pong.conf '/etc/init/pong.conf'
+
     cd '/usr/share'
     git clone 'https://github.com/etsy/statsd.git'
     cd '/usr/share/statsd'
     npm install
 
-    cp "${appPath}/config/config.js" '/etc/statsd/localConfig.js'
-
-    cp "${appPath}/files/statsd.conf" '/etc/init/statsd.conf'
+    cd '/usr/share'
+    git clone 'https://github.com/mapbox/pong.git'
+    cd '/usr/share/pong'
+    npm install
 }
 
 function startServers
 {
     service statsd start
+    service pong start
 }
 
 function main
